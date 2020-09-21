2fb932f79fbd88c1743ddaeaa21a8cc09d93b869
Crowdbotics <team@crowdbotics.com>
2020-09-21 20:19:37 +0000

    Update models

--- backend/hammer_20527/wsgi.py
+++ backend/hammer_20527/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hammer_20527.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hammer_20527.settings")
 
 application = get_wsgi_application()
